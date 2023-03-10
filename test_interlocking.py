e2dca77ed76f09f06661ff2430e7570fa3269aa5
Dirk Friedenberger <Dirk.Friedenberger@deutschebahn.com>
2023-03-10 10:18:25 +0100

    update current state

--- test_interlocking.py
+++ test_interlocking.py
@@ -1,11 +1,12 @@
 
 from interlocking import Interlocking
 from metadatacontroller import MetadataController
+from model import Train
 
 def test_01():
 
     metadata_controller = MetadataController()
-    metadata_controller.load_metadata('data/Lausitz-LEAG.metadata.json ')
+    metadata_controller.load_metadata('test/Lausitz-LEAG.metadata.json ')
 
     def move_point_callback(point_id,orientation):
         print(f"Move Point {point_id} {orientation}")
@@ -14,8 +15,8 @@ def test_01():
     def set_signal_state_callback(signal_id,state,wirkrichtung):
         print(f"Set Signal {signal_id} {state} {wirkrichtung} ")
 
-    interlocking = Interlocking('data/Lausitz-LEAG',move_point_callback,set_signal_state_callback)
-    interlocking.stations = metadata_controller.stations
+    interlocking = Interlocking('test/MVP.routen',move_point_callback,set_signal_state_callback)
+    #interlocking.stations = metadata_controller.stations
 
     interlocking.prepare([])
 
@@ -27,11 +28,17 @@ def test_01():
     for route in interlocking.routes:
         print(f"{route.to_string()}\t(ID: {route.id}; Available in SUMO: {route.available_in_sumo}; UUID: {route.route_uuid})")
 
+    test_train = Train("test")
+    test_train.max_speed = 60 # > 60 results in IndexError: list index out of range
 
-    #traindetection
-    #interlocking.train_detection_controller.count_in("roadid")
+    for test_route in interlocking.routes:
 
-    #interlocking.print_state()
+        assert interlocking.set_route(test_route,test_train) == True
+        
+        interlocking.free_route(test_route)
+            
+
+    interlocking.print_state()
 
    
 
