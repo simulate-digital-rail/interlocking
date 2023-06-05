5307b6a6b9d2abd0a549f7d86b56fe749278a25f
Arne Boockmeyer <github@arneboockmeyer.de>
2023-06-05 14:43:30 +0200

    Add SUMO example

--- interlocking/infrastructureprovider/sumoinfrastructureprovider.py
+++ interlocking/infrastructureprovider/sumoinfrastructureprovider.py
@@ -10,13 +10,14 @@ class SUMOInfrastructureProvider(InfrastructureProvider):
 
     async def set_signal_state(self, yaramo_signal, target_state):
         if target_state == "go":
-            self.traci_instance.setRedYellowGreenState(yaramo_signal.name, "GG")
+            self.traci_instance.trafficlight.setRedYellowGreenState(yaramo_signal.name, "GG")
         elif target_state == "halt":
             if yaramo_signal.direction == SignalDirection.IN:
-                self.traci_instance.setRedYellowGreenState(yaramo_signal.name, "rG")
+                self.traci_instance.trafficlight.setRedYellowGreenState(yaramo_signal.name, "rG")
             else:
-                self.traci_instance.setRedYellowGreenState(yaramo_signal.name, "Gr")
+                self.traci_instance.trafficlight.setRedYellowGreenState(yaramo_signal.name, "Gr")
+        return True
 
     async def turn_point(self, yaramo_point: str, target_orientation: str):
         # SUMO does not support the concept of points yet.
-        pass
+        return True
---
+++ sumo_example.py
@@ -0,0 +1,96 @@
+from planpro_importer.reader import PlanProReader
+from railwayroutegenerator.routegenerator import RouteGenerator
+from sumoexporter import SUMOExporter
+from interlocking.interlockinginterface import Interlocking
+from interlocking.model.helper import Settings
+from interlocking.infrastructureprovider import LoggingInfrastructureProvider, SUMOInfrastructureProvider, RandomWaitInfrastructureProvider
+import asyncio
+import logging
+import traci
+from pathlib import Path
+
+
+def prepare_yaramo_topology():
+    path = __file__.split("sumo_example.py")[0]
+    reader = PlanProReader(path + "/test/complex-example")
+    topology = reader.read_topology_from_plan_pro_file()
+
+    # TODO: At the moment, the railway route generator can not calculate max speeds since the edges have no max speeds
+    outer_station_edges = ["88d820fc-95b4-408c-b418-a516877de139", "66adf559-7dd5-487f-bad7-8e256a8a8f44",
+                           "f438d162-b5c4-4c0c-9d73-f55fad294742", "61d57b1b-2ae2-4637-897b-abbab41b8e69",
+                           "f63e5398-f20c-47be-bbed-f7f8f8034bff"]
+    for edge_uuid in topology.edges:
+        edge = topology.edges[edge_uuid]
+        if edge_uuid in outer_station_edges:
+            edge.maximum_speed = 60
+        else:
+            edge.maximum_speed = 40
+
+    generator = RouteGenerator(topology)
+    generator.generate_routes()
+    return topology
+
+
+def get_route_by_signal_names(start_signal_name: str, end_signal_name: str, topology):
+    for route_uuid in topology.routes:
+        route = topology.routes[route_uuid]
+        if route.start_signal.name == start_signal_name and route.end_signal.name == end_signal_name:
+            return route
+    return None
+
+
+def sumo_test():
+    topology = prepare_yaramo_topology()
+
+    traci.init(host="localhost", port=4444)
+
+    infrastructure_provider = [LoggingInfrastructureProvider(), RandomWaitInfrastructureProvider(allow_fail=False), SUMOInfrastructureProvider(traci_instance=traci)]
+
+    interlocking = Interlocking(infrastructure_provider, Settings(max_number_of_points_at_same_time=3))
+    interlocking.prepare(topology)
+    interlocking.print_state()
+
+    start_signal_name = "60AS3"
+    end_signal_name = "60BS7"
+
+    yaramo_route = get_route_by_signal_names(start_signal_name, end_signal_name, topology)
+    asyncio.run(interlocking.set_route(yaramo_route))
+    traci.vehicle.add("Zug1", f"route_{start_signal_name}-{end_signal_name}", "regio")
+    traci.simulationStep()
+    position = traci.vehicle.getRoadID("Zug1")
+    if position.endswith("-re"):
+        position = position[:-3]
+    infrastructure_provider[1].tds_count_in(position)
+    while len(traci.vehicle.getIDList()) > 0:
+        traci.simulationStep()
+        if len(traci.vehicle.getIDList()) == 0:
+            break
+        new_position = traci.vehicle.getRoadID("Zug1")
+        if not new_position.startswith(":"):
+            if new_position.endswith("-re"):
+                new_position = new_position[:-3]
+            if new_position != position:
+                infrastructure_provider[1].tds_count_out(position)
+                position = new_position
+                infrastructure_provider[1].tds_count_in(position)
+        asyncio.run(asyncio.sleep(traci.simulation.getDeltaT()))
+
+
+def create_sumo_scenario():
+    topology = prepare_yaramo_topology()
+    sumo_exporter = SUMOExporter(topology)
+    sumo_exporter.convert()
+    sumo_exporter.write_output()
+    logging.info("Start sumo with:")
+    logging.info("")
+    logging.info("sumo-gui -c sumo-config/complex-example.scenario.sumocfg --remote-port 4444 --step-length=0.1 -S")
+    logging.info("or")
+    logging.info("sumo -c sumo-config/complex-example.scenario.sumocfg --remote-port 4444 --step-length=0.1")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    if Path("./sumo-config/complex-example.scenario.sumocfg").is_file():
+        sumo_test()
+    else:
+        create_sumo_scenario()
