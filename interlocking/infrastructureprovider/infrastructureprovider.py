597562855fa5e46d1e183ce20aef56ec0aef78b9
Arne Boockmeyer <github@arneboockmeyer.de>
2023-07-26 16:42:04 +0200

    Add function to verify that all elements are covered by some infrastructure provider (and tests for that)

--- interlocking/infrastructureprovider/infrastructureprovider.py
+++ interlocking/infrastructureprovider/infrastructureprovider.py
@@ -1,14 +1,23 @@
+import logging
 from abc import ABC, abstractmethod
-from yaramo.model import Node, Signal
+from yaramo.model import Node, Signal, Topology
 
 
 class InfrastructureProvider(ABC):
 
     def __init__(self,
+                 apply_for_signals: bool = True,
+                 apply_for_points: bool = True,
                  only_apply_for_signals: list[str] = None,
                  only_apply_for_points: list[str] = None,
                  apply_for_all_signals_except: list[str] = None,
                  apply_for_all_points_except: list[str] = None):
+        self.apply_for_signals = apply_for_signals
+        self.apply_for_points = apply_for_points
+
+        if not self.apply_for_points and not self.apply_for_signals:
+            raise Exception("The infrastructure provider has to apply for signals, points or both.")
+
         if only_apply_for_signals is None:
             only_apply_for_signals = []
         self.only_apply_for_signals = only_apply_for_signals
@@ -36,13 +45,16 @@ class InfrastructureProvider(ABC):
     # Point Interaction
     #
 
-    async def call_turn_point(self, yaramo_point: Node, target_orientation: str):
+    def is_point_covered(self, yaramo_point: Node):
+        if not self.apply_for_points:
+            return False
         point_id = yaramo_point.uuid[-5:]
-        if point_id in self.only_apply_for_points:
-            return await self.turn_point(yaramo_point, target_orientation)
-        if len(self.only_apply_for_points) == 0 and point_id not in self.apply_for_all_points_except:
-            return await self.turn_point(yaramo_point, target_orientation)
+        return point_id in self.only_apply_for_points or \
+            (len(self.only_apply_for_points) == 0 and point_id not in self.apply_for_all_points_except)
 
+    async def call_turn_point(self, yaramo_point: Node, target_orientation: str):
+        if self.is_point_covered(yaramo_point):
+            return await self.turn_point(yaramo_point, target_orientation)
         # return True to skip this call and not prevent successfully turning of point.
         return True
 
@@ -56,13 +68,16 @@ class InfrastructureProvider(ABC):
     # Signal Interaction
     #
 
-    async def call_set_signal_state(self, yaramo_signal: Signal, target_state: str):
-        if yaramo_signal.name in self.only_apply_for_signals:
-            return await self.set_signal_state(yaramo_signal, target_state)
-        if len(self.only_apply_for_signals) == 0 and yaramo_signal.name not in self.apply_for_all_signals_except:
-            return await self.set_signal_state(yaramo_signal, target_state)
+    def is_signal_covered(self, yaramo_signal: Signal):
+        if not self.apply_for_signals:
+            return False
+        return yaramo_signal.name in self.only_apply_for_signals or \
+            (len(self.only_apply_for_signals) == 0 and yaramo_signal.name not in self.apply_for_all_signals_except)
 
-        # return True to skip this call and not prevent successfully turning of point.
+    async def call_set_signal_state(self, yaramo_signal: Signal, target_state: str):
+        if self.is_signal_covered(yaramo_signal):
+            return await self.set_signal_state(yaramo_signal, target_state)
+        # return True to skip this call and not prevent successfully turning of signal.
         return True
 
     @abstractmethod
@@ -89,3 +104,35 @@ class InfrastructureProvider(ABC):
     async def tds_count_out(self, track_segment_id, train_id: str):
         """Removes a train to the segment identified by the `segment_id`"""
         self.tds_count_out_callback(track_segment_id, train_id)
+
+    #
+    # Verify that all elements are covered by some infrastructure providers and add a default provider if not
+    #
+
+    @staticmethod
+    def verify_all_elements_covered_by_infrastructure_provider(topology: Topology, infrastructure_providers, settings):
+        uncovered_signals = []
+        uncovered_points = []
+
+        for signal in list(topology.signals.values()):
+            if not any(ip.is_signal_covered(signal) for ip in infrastructure_providers):
+                logging.warning(f"The signal {signal.name} is not covered by any infrastructure provider.")
+                uncovered_signals.append(signal.name)
+
+        for point in list(topology.nodes.values()):
+            if len(point.connected_nodes) != 3:
+                continue  # Skip all track ends
+            if not any(ip.is_point_covered(point) for ip in infrastructure_providers):
+                point_id = point.uuid[-5:]
+                logging.warning(f"The point {point_id} is not covered by any infrastructure provider.")
+                uncovered_points.append(point_id)
+
+        if len(uncovered_signals) > 0 or len(uncovered_points) > 0:
+            if settings.default_interlocking_provider is not None:
+                return settings.default_interlocking_provider(apply_for_points=len(uncovered_points) > 0,
+                                                              apply_for_signals=len(uncovered_signals) > 0,
+                                                              only_apply_for_points=uncovered_points,
+                                                              only_apply_for_signals=uncovered_signals)
+        return None
+
+
--- test/infrastructureprovider_test.py
+++ test/infrastructureprovider_test.py
@@ -1,4 +1,7 @@
 from .helper import topologyhelper, interlockinghelper, trackoperationsinfrastructureprovider
+from interlocking.interlockinginterface import Interlocking
+from interlocking.infrastructureprovider import LoggingInfrastructureProvider
+from interlocking.model.helper import Settings
 import asyncio
 import unittest
 
@@ -26,6 +29,42 @@ class TestUsingBothLimiters(unittest.TestCase):
         self.assertEqual(str(exception.exception), f"You can not limit the infrastructure provider with "
                                                    f"only_apply_for_points and apply_for_all_points_except at "
                                                    f"the same time.")
+
+
+class TestApplyForNone(unittest.TestCase):
+
+    def test_apply_for_none(self):
+        with self.assertRaises(Exception) as exception:
+            ip = LoggingInfrastructureProvider(apply_for_points=False, apply_for_signals=False)
+
+        self.assertEqual(str(exception.exception), f"The infrastructure provider has to apply for "
+                                                   f"signals, points or both.")
+
+
+def test_apply_for_signals():
+    topology = topologyhelper.get_topology_from_planpro_file("./complex-example.ppxml")
+    ip = trackoperationsinfrastructureprovider.TrackOperationsInfrastructureProvider(apply_for_signals=False)
+    interlocking = interlockinghelper.get_interlocking(topology, infrastructure_provider=[ip])
+    route_es1_as1 = topologyhelper.get_route_by_signal_names(topology, "60ES1", "60AS1")
+    asyncio.run(interlockinghelper.set_route(interlocking, route_es1_as1, True, "RB101"))
+
+    signal_es1 = interlockinghelper.get_interlocking_signal_by_name(interlocking, "60ES1").yaramo_signal
+    point_on_first_route = interlockinghelper.get_interlocking_point_by_id(interlocking, "fd73d").yaramo_node
+    assert not ip.was_signal_set_to_aspect(signal_es1, "go")
+    assert ip.was_point_turned_to(point_on_first_route, "right")
+
+
+def test_apply_for_points():
+    topology = topologyhelper.get_topology_from_planpro_file("./complex-example.ppxml")
+    ip = trackoperationsinfrastructureprovider.TrackOperationsInfrastructureProvider(apply_for_points=False)
+    interlocking = interlockinghelper.get_interlocking(topology, infrastructure_provider=[ip])
+    route_es1_as1 = topologyhelper.get_route_by_signal_names(topology, "60ES1", "60AS1")
+    asyncio.run(interlockinghelper.set_route(interlocking, route_es1_as1, True, "RB101"))
+
+    signal_es1 = interlockinghelper.get_interlocking_signal_by_name(interlocking, "60ES1").yaramo_signal
+    point_on_first_route = interlockinghelper.get_interlocking_point_by_id(interlocking, "fd73d").yaramo_node
+    assert ip.was_signal_set_to_aspect(signal_es1, "go")
+    assert not ip.was_point_turned_to(point_on_first_route, "right")
 
 
 def test_only_apply_for():
@@ -72,3 +111,42 @@ def test_apply_for_all_except():
     assert ip.was_point_turned_to(point_on_first_route_fa9ea, "left")
     assert ip.was_point_turned_to(point_on_first_route_21b88, "right")
     assert ip.was_point_turned_to(point_on_second_route, "right")
+
+
+def test_infrastructure_providers_cover_all_elements():
+    topology = topologyhelper.get_topology_from_planpro_file("./simple-example.ppxml")
+    ip = LoggingInfrastructureProvider(
+        only_apply_for_signals=["A1", "B1"],
+        only_apply_for_points=["d2c77"]
+    )
+    interlocking = Interlocking(ip)
+    assert len(interlocking.infrastructure_providers) == 1
+    assert ip.is_point_covered(topologyhelper.get_point_by_name(topology, "d2c77"))
+    assert not ip.is_point_covered(topologyhelper.get_point_by_name(topology, "c92aa"))
+    assert ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "A1"))
+    assert ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "B1"))
+    assert not ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "A2"))
+    assert not ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "B2"))
+
+    interlocking.prepare(topology)
+    assert len(interlocking.infrastructure_providers) == 2
+
+    new_ip = interlocking.infrastructure_providers[1]
+    assert not new_ip.is_point_covered(topologyhelper.get_point_by_name(topology, "d2c77"))
+    assert new_ip.is_point_covered(topologyhelper.get_point_by_name(topology, "c92aa"))
+    assert not new_ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "A1"))
+    assert not new_ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "B1"))
+    assert new_ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "A2"))
+    assert new_ip.is_signal_covered(topologyhelper.get_signal_by_name(topology, "B2"))
+
+
+def test_no_default_infrastructure_provider():
+    topology = topologyhelper.get_topology_from_planpro_file("./simple-example.ppxml")
+    ip = LoggingInfrastructureProvider(
+        only_apply_for_signals=["A1", "B1"],
+        only_apply_for_points=["d2c77"]
+    )
+    interlocking = Interlocking(ip, Settings(default_interlocking_provider=None))
+    assert len(interlocking.infrastructure_providers) == 1
+    interlocking.prepare(topology)
+    assert len(interlocking.infrastructure_providers) == 1
